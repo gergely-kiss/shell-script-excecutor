c577fbe2fe907f8485c2ec96962da8495e365dca
kiss-gergely@outlook.com <ag0OO5xC8D66mwFo>
2020-05-14 12:56:11 +0100

    init

---
+++ src/test/java/uk/gergely/kiss/shellscriptexecutor/ShellScriptExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.shellscriptexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellScriptExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
